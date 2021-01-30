1c9744c84e0cee5e1a2af1106662291a7acae43c
AricanoMX <aricanomx@gmail.com>
2021-01-30 13:17:55 -0600

    add new informatin about react hookso

---
+++ src/components/Map.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = ({ data }) => {
+  const mapStyles = {
+    height: '25vh',
+    width: '100%',
+  };
+
+  const defaultCenter = {
+    lat: data.lat,
+    lng: data.lng,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyDFxzus_G2ssQaYfNExQi2kP91VLM9auqQ">
+      <GoogleMap mapContainerStyle={mapStyles} zoom={10} center={defaultCenter}>
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
